c95b5c093606c951d12b071818a78c2d070a0aa3
Cory Reed <swashcap@gmail.com>
2014-08-30 16:23:26 -0700

    Add root application file.

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,6 @@
+/**
+ *= require vendor/fastclick/lib/fastclick.js
+ *= require vendor/angular/angular.js
+ *= require vendor/angular-sanitize/angular-sanitize.js
+ *= require vendor/angular-ui-router/release/angular-ui-router.js
+ */
