7c5d42b476076d45159c0b75da3b8bca700ead4d
Cory Reed <swashcap@gmail.com>
2014-08-30 16:54:38 -0700

    Add basic templates and controllers.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,3 +9,44 @@
  *= require angular-rails-templates
  *= require_tree ../templates
  */
+
+'use strict';
+
+angular.module('attendance', ['ngSanitize', 'ui.router', 'templates'])
+  .config(['$stateProvider', function ($stateProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'home.html',
+        controller: 'HomeCtrl'
+      })
+      .state('classroom-period', {
+        url: '/classroom-period/:id',
+        templateUrl: 'classroom-period.html',
+        controller: 'ClassroomPeriodCtrl'
+      })
+      .state('student', {
+        url: '/student/:id',
+        templateUrl: 'student.html',
+        controller: 'StudentCtrl'
+      });
+  }])
+  .config(['$urlRouterProvider', function ($urlRouterProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+  }]);
+
+angular.module('attendance')
+  .controller('HomeCtrl', ['$scope', '$http', function ($scope, $http) {
+    $scope.message = 'Hello';
+  }]);
+
+angular.module('attendance')
+  .controller('ClassroomPeriodCtrl', function ($scope) {
+    $scope.message = 'Hello';
+  });
+
+angular.module('attendance')
+  .controller('StudentCtrl', ['$scope', function ($scope) {
+
+  }]);
