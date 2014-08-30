ec976555f6e1e7196739f312c33ac88ff0b9b0ac
Cory Reed <swashcap@gmail.com>
2014-08-30 16:26:08 -0700

    Add 'angular-rails-templates' to root script file.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,4 +3,9 @@
  *= require vendor/angular/angular.js
  *= require vendor/angular-sanitize/angular-sanitize.js
  *= require vendor/angular-ui-router/release/angular-ui-router.js
+ *
+ * Get all the Angular templates
+ * (https://github.com/pitr/angular-rails-templates)
+ *= require angular-rails-templates
+ *= require_tree ../templates
  */
