4270ad5b2550dfac1bfe4478254e16483c91d57c
David La Chasse <david.lachasse@gmail.com>
2014-08-30 17:25:47 -0700

    Build users#index endpoint

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,9 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 		respond_with @user.to_json(except: [:created_at, :updated_at])
 	end
+
+	def index
+		@users = User.all
+		respond_with @users.to_json(except: [:created_at, :updated_at])
+	end
 end
