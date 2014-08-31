b9f133ff4d2b90577ead0e77d10b2531b1fb5e10
David La Chasse <david.lachasse@gmail.com>
2014-08-30 18:36:31 -0700

    Update attendance data output

--- app/models/classroom_period.rb
+++ app/models/classroom_period.rb
@@ -1,6 +1,7 @@
 class ClassroomPeriod < ActiveRecord::Base
 	has_many :presences, through: :schedules
 	has_many :schedules
+	has_one :course
 	has_many :users, through: :schedules
 	# has_one :teacher_id, foreign_key: 'user_id', class_name: 'users'
 end
