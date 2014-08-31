8f8f750e968ab1799a34865e23eb7dbecf08fb73
Cory Reed <swashcap@gmail.com>
2014-08-30 17:37:58 -0700

    Add 'Users' view.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,6 +25,11 @@ angular.module('attendance', ['ngSanitize', 'ui.router', 'templates'])
         templateUrl: 'classroom-period.html',
         controller: 'ClassroomPeriodCtrl'
       })
+      .state('users', {
+        url: '/users',
+        templateUrl: 'users.html',
+        controller: 'UsersCtrl'
+      })
       .state('student', {
         url: '/student/:id',
         templateUrl: 'student.html',
@@ -47,6 +52,45 @@ angular.module('attendance')
   });
 
 angular.module('attendance')
-  .controller('StudentCtrl', ['$scope', function ($scope) {
+  .controller('UsersCtrl', function ($scope, $http) {
+    $http.get('/api/users')
+      .then(function (data) {
+        var users = data.data.sort(function (a, b) {
+          if (a.last_name > b.last_name) {
+            return 1;
+          } else if (a.last_name < b.last_name) {
+            return -1;
+          }
+           return 0;
+        });
 
-  }]);
+        $scope.users = users;
+      });
+  });
+
+angular.module('attendance')
+  .controller('StudentCtrl', function ($scope, $stateParams, $http) {
+    // Get student records
+    var studentId = $stateParams.id;
+
+    if (typeof studentId !== 'undefined') {
+      $http.get('/api/users/' + studentId)
+        .then(function (data) {
+          $scope.user = data.data;
+        });
+      $http.get('/api/users/' + studentId + '/classroom_periods')
+        .then(function (data) {
+          var courses = data.data.sort(function (a, b) {
+            if (a.course_id > b.course_id) {
+              return 1;
+            } else if (a.course_id < b.course_id) {
+              return -1;
+            }
+            return 0;
+          })
+
+          $scope.courses = courses;
+        });
+    }
+
+  });
